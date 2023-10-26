7234a93cfa8276c85be46ee7ddccd57ef02163ed
natex <natexsweet@gmail.com>
2023-10-26 14:44:08 -0700

    co4 in progress

---
+++ cploration/c04/parser.c
@@ -0,0 +1,51 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] $YOUR_NAME$
+ * [TERM] FALL $YEAR$
+ * 
+ ****************************************/
+#include "parser.h"
+
+/* Function: strip
+ * -------------
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s){	
+	char *s_new[sizeof(s)+1] ={0};
+	int i = 0;
+	for (char *s2 = s; *s2; s2++) { 
+		if (*s2 =='/' && *(s2+1)=='/'){
+			break;
+		}else if (isspace(*s2)){
+			s_new[i++]=*s2;
+		}
+	}
+	s_new[i]='\0';
+	strcpy(s,s_new);
+    return s;	
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments. 
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE * file){
+	char line[MAX_LINE_LENGTH] = {0};
+	unasigned int line_num = 0;
+	
+	While (fgets(line,sizeof(line),file)){
+		strip(line)
+		if (!*line){
+			printf("%s",line);
+		}
+	}
+}
