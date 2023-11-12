9d5bd72afee56dc06acd842a43d49d9d7753f900
natex <natexsweet@gmail.com>
2023-11-12 12:12:28 -0800

    fill.asm complete

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -10,5 +10,50 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+ @x // starting point
+  M=0 // x at upper left corner
 
-// Put your code here.
+(loop)
+@KBD 
+D=M 
+@black
+D;JGT
+@white
+0;JMP 
+
+(black)
+@x
+D=M
+@8192 
+D=D-A
+@loop
+D;JGE 
+@x
+D=M
+@SCREEN
+A=A+D 
+M=-1 
+@x
+M=M+1 
+@loop
+0;JMP 
+ 
+(white)
+@x
+D=M 
+@loop
+D;JLT 
+@x
+D=M
+@SCREEN
+A=A+D 
+M=0 
+@x
+M=M-1 
+@loop
+0;JMP 
+
+
+(end)
+@end
+0;JMP 
